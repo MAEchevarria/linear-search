194c16d7125b9218bb33728ea0493e07f5730c21
Michael Echevarria <michael.echevarria@live.com>
2022-05-25 23:21:43 -0500

    updated code for js and py

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,23 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+    if (arrayToSearchThrough.indexOf(valueToFind) != -1) {
+        return arrayToSearchThrough.indexOf(valueToFind);
+    } else {
+        return undefined;
+    }
+}
 
-};
-
+exports.linearSearchGlobal = function(valueToFind, arrayToSearchThrough) {
+  let arr = [];
+  for (let i = 0; i < arrayToSearchThrough.length; i++) {
+    if (arrayToSearchThrough[i] === valueToFind) {
+      arr.push(i);
+    }
+  }
+  if (arr.length === 0) {
+    return undefined;
+  } else {
+    return arr;
+  }
+}
--- python/linear_search.py
+++ python/linear_search.py
@@ -2,5 +2,20 @@ array_to_search_through = []
 for number in range(1, 1001):
     array_to_search_through.append(number)
 
-def linear_search(value_to_find, array_to_search_through):
-    # your code here
+def linear_search(num, num_list):
+    for i, x in enumerate(num_list):
+        if x == num:
+            return i
+    else:
+        return None
+
+def global_linear_search(num, num_list):
+    list = []
+    for i, x in enumerate(num_list):
+        if x == num:
+            list.append(i)
+    if bool(list) == False:
+        return "None"
+    else:
+        return list
+        
